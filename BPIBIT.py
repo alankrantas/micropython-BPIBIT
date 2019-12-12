d834dbc4f92273f2b7a62a6cdb3007b557eea8f5
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2019-12-12 14:18:23 +0800

    Update BPIBIT.py

--- BPIBIT.py
+++ BPIBIT.py
@@ -15,7 +15,7 @@ _analogPins = {3:13, 0:25, 4:16, 5:35, 6:12, 7:14, 1:32, 10:26, 11:27, 12:2, 2:3
 _digitalPins = {3:13, 0:25, 4:16, 5:35, 6:12, 7:14, 1:32, 8:16, 9:17, 10:26, 11:27, 12:2, 2:33, 13:18, 14:19, 15:23, 16:5, 19:22, 20:21}
 _touchpads = {3:13, 6:12, 7:14, 1:32, 11:27, 2:33}
 _ledScreen = {0:4, 1:9, 2:14, 3:19, 4:24, 5:3, 6:8, 7:13, 8:18, 9:23, 10:2, 11:7, 12:12, 13:17, 14:22, 15:1, 16:6, 17:11, 18:16, 19:21, 20:0, 21:5, 22:10, 23:15, 24:20}
-_colorCodes = {'W':(16, 16, 16), 'R':(48, 0, 0), 'G':(0, 48, 0), 'B':(0, 0, 48), 'Y':(24, 24, 0), 'C':(0, 24, 24), 'P':(24, 0, 24), 'O':(36, 12, 0), 'V':(12, 0, 36), '*':(0, 0, 0)}
+_colorCodes = {'W':(16, 16, 16), 'R':(48, 0, 0), 'G':(0, 48, 0), 'B':(0, 0, 48), 'Y':(24, 24, 0), 'C':(0, 24, 24), 'P':(24, 0, 24), 'O':(36, 12, 0), 'T':(0, 36, 12), 'V':(12, 0, 36), '*':(0, 0, 0)}
 _tones = {'C3':130.8128, 'C3D3':138.5913, 'D3':146.8324, 'D3E3':155.5635, 'E3':164.8138, 'F3':174.6141, 'F3G3':184.9972, 'G3':195.9977, 'G3A3':207.6523, 'A3':220.0000, 'A3B3':233.0819, 'B3':246.9417, 'C4':261.6256, 'C4D4':277.1826, 'D4':293.6648, 'D4E4':311.1270, 'E4':329.6276, 'F4':349.2282, 'F4G4':369.9944, 'G4':391.9954, 'G4A4':415.3047, 'A4':440.0000, 'A4B4':466.1638, 'B4':493.8833, 'C5':523.2511, 'C5D5':554.3653, 'D5':587.3295, 'D5E5':622.2540, 'E5':659.2551, 'F5':698.4565, 'F5G5':739.9888, 'G5':783.9909, 'G5A5':830.6094, 'A5':880.0000, 'A5B5':932.3275, 'B5':987.7666,  'C6':1046.502, 'C6D6':1108.731, 'D6':1174.659, 'D6E6':1244.508, 'E6':1318.510, 'F6':1396.913, 'F6G6':1479.978, 'G6':1567.982, 'G6A6':1661.219, 'A6':1760.000, 'A6B6':1864.655, 'B6':1975.533, 'C7':2093.005, '*': 0.0, ' ': 0.0}
 _axisName = {'x':0, 'y':1, 'z':2}
 _fonts = {'A':['*', 'X', 'X', '*', '*', 'X', '*', '*', 'X', '*', 'X', 'X', 'X', 'X', '*', 'X', '*', '*', 'X', '*', 'X', '*', '*', 'X', '*'],
@@ -118,7 +118,7 @@ LED = 18
 def getI2C(scl=19, sda=20, freq=400000):
     return I2C(scl=Pin(_digitalPins[scl]), sda=Pin(_digitalPins[sda]), freq=freq)
 
-def getSPI(sck=13, miso=14, mosi=15, baudrate=100000, polarity=1, phase=0):
+def getSPI(sck=13, miso=14, mosi=15, baudrate=1000000, polarity=1, phase=0):
     return SPI(baudrate=baudrate, polarity=polarity, phase=phase, sck=Pin(_digitalPins[sck]), mosi=Pin(_digitalPins[mosi]), miso=Pin(_digitalPins[miso]))
 
 def getHSPI(baudrate=10000000, polarity=1, phase=0):
@@ -127,8 +127,11 @@ def getHSPI(baudrate=10000000, polarity=1, phase=0):
 def getVSPI(baudrate=10000000, polarity=1, phase=0):
     return SPI(2, baudrate=baudrate, polarity=polarity, phase=phase, sck=Pin(_digitalPins[13]), mosi=Pin(_digitalPins[15]), miso=Pin(_digitalPins[14]))
 
-def pause(delay=100):
+def pause(delay):
     utime.sleep_ms(delay)
+    
+def pauseMicros(delay):
+    utime.sleep_us(delay)
 
 def runningTime():
     return utime.ticks_ms()
@@ -136,13 +139,10 @@ def runningTime():
 def runningTimeMicros():
     return utime.ticks_us()
 
-def digitalPin(pin):
-    return _digitalPins[pin]
-
 def digitalReadPin(pin):
     return Pin(_digitalPins[pin], Pin.IN).value()
 
-def digitalWritePin(pin, value=0):
+def digitalWritePin(pin, value):
     Pin(_digitalPins[pin], Pin.OUT).value(value)
 
 def analogReadPin(pin):
@@ -150,10 +150,10 @@ def analogReadPin(pin):
     adc.atten(ADC.ATTN_11DB)
     return adc.read() // 4
 
-def analogWritePin(pin, value=0):
+def analogWritePin(pin, value):
     pwm = PWM(Pin(_digitalPins[pin]), freq=5000, duty=value)
 
-def servoWritePin(pin, degree=90):
+def servoWritePin(pin, degree):
     actual_degree = int(degree * (122 - 30) / 180 + 30)
     servo = PWM(Pin(_digitalPins[pin]), freq=50, duty=actual_degree)
 
@@ -179,7 +179,7 @@ _analogPitchPin = 0
 def analogSetPitchPin(pin):
     _analogPitchPin = pin
 
-def analogPitch(freq=0, delay=0):
+def analogPitch(freq, delay):
     buzzer = PWM(Pin(_digitalPins[_analogPitchPin], Pin.OUT), freq=round(freq), duty=512)
     if delay > 0:
         if delay > 10:
@@ -190,7 +190,7 @@ def analogPitch(freq=0, delay=0):
             pause(delay)
             buzzer.deinit()
 
-def playTone(note='-', delay=0):
+def playTone(note, delay=0):
     analogPitch(freq=_tones[note], delay=delay)
 
 def rest(delay):
@@ -293,7 +293,7 @@ def ledCodeArray(array):
 def ledOff():
     ledCodeAll('*')
 
-def plotBarGraph(value=0, maxValue=0, code='W'):
+def plotBarGraph(value, maxValue=1023, code='W'):
     p = value / maxValue
     valueArray = [0.96, 0.88, 0.84, 0.92, 1.00, 0.76, 0.68, 0.64, 0.72, 0.80, 0.56, 0.48, 0.44, 0.52, 0.60, 0.36, 0.28, 0.24, 0.32, 0.40, 0.16, 0.08, 0.04, 0.12, 0.20]
     ledArray = []
